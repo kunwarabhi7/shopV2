8eb4f02efb4828a9e0b8888a0dbe36ae201beaf4
Abhishek <abhishekparmar162@gail.com>
2022-09-12 15:48:52 +0530

    added shop data json

--- src/App.js
+++ src/App.js
@@ -2,13 +2,9 @@ import { Route, Routes } from 'react-router-dom';
 import Home from './routes/home/home.component';
 import NavBar from './routes/navigation/navigation.component';
 import Authentication from './routes/authentication/authentication.component';
+import Shop from './routes/shop/shop.component';
 
 
-const Shop = () => {
-  return (
-    <h1>I am the Shop Page</h1>
-  )
-}
 const App = () => {
 
 
